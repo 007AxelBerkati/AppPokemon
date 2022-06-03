000af7587fef774d842e11c794f84b111fa263c1
007AxelBerkati <bucel1916@gmail.com>
2022-06-03 16:04:11 +0700

    update

--- src/pages/SplashScreen/index.js
+++ src/pages/SplashScreen/index.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import {
   StyleSheet, Text, View, Image,
 } from 'react-native';
-import auth from '@react-native-firebase/auth';
+// import auth from '@react-native-firebase/auth';
 import {
   colors, fonts, onLogScreenView, windowHeight, windowWidth,
 } from '../../utils';
