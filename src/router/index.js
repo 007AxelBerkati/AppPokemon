d7876a3da46398c5308964e36e09628e87959117
007AxelBerkati <bucel1916@gmail.com>
2022-06-09 19:33:16 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -17,24 +17,14 @@ function Router() {
 
   useEffect(() => {
     messaging().onNotificationOpenedApp((remoteMessage) => {
-      // console.log(
-      //   'Notification caused app to open from background state:',
-      //   remoteMessage.notification,
-      // );
       navigate(remoteMessage.data.type);
     });
 
-    // Check whether an initial notification is available
     messaging()
       .getInitialNotification()
       .then((remoteMessage) => {
         if (remoteMessage) {
-          // console.log(
-          //   'Notification caused app to open from quit state:',
-          //   remoteMessage.notification,
-          // );
-          // console.log('remoteMessage.data.type : ', remoteMessage.data.type);
-          setInitialRoute(remoteMessage.data.type); // e.g. "Settings"
+          setInitialRoute(remoteMessage.data.type);
         }
         setLoading(false);
       });
